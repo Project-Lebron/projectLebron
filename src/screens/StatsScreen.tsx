3784804a26a1a702d2b7a3b792792478f3dc9b16
zyrephus <144397070+zyrephus@users.noreply.github.com>
2023-11-11 17:27:29 -0500

    Made title and background StatsScreen.tsx

--- src/screens/StatsScreen.tsx
+++ src/screens/StatsScreen.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import React from 'react';
 
 const StatsScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>Stats</Text>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={'light-content'} />
+      <Text style={styles.titleScreen}>Stats</Text>
     </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0D1B2A',
+    alignItems: 'center',
+  },
+  titleScreen: {
+    color:'white',
+    fontFamily: 'Arial',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+})
 
 export default StatsScreen
