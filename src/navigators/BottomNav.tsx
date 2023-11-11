86e903a622199bfd6062226da620ca0a0c34f823
stevenyang <steventanyang@gmail.com>
2023-11-11 17:33:29 -0500

    update

--- src/navigators/BottomNav.tsx
+++ src/navigators/BottomNav.tsx
@@ -6,6 +6,8 @@ import HomeScreen from "../screens/HomeScreen";
 import StatsScreen from "../screens/StatsScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 
+// https://fonts.google.com/icons?selected=Material+Icons:sports_basketball:&icon.query=basketball
+
 const Tab = createBottomTabNavigator();
 
 const BottomNav = () => {
@@ -15,7 +17,9 @@ const BottomNav = () => {
             headerShown: false,
         	tabBarHideOnKeyboard: true,
             tabBarShowLabel: false,
+            tabBarStyle:styles.tabBarStyle,
         }}>
+        
       <Tab.Screen name="Stats" component={StatsScreen}></Tab.Screen>
       <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
       <Tab.Screen name="Profile" component={ProfileScreen}></Tab.Screen>
@@ -23,6 +27,19 @@ const BottomNav = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+	tabBarStyle: {
+		height:80,
+		position:'absolute',
+		backgroundColor:'#ddd',
+		borderTopwidth:0,
+		elevation:0,
+		borderTopColor:'transparent',
+	}
+
+});
+
+
 
 export default BottomNav;
