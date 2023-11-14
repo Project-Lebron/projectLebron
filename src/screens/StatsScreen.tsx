adf2a5edcb1bb76bc1b81b6361fc428f4e868429
Zyrephus <wilsonchen315@gmail.com>
2023-11-14 13:33:21 -0500

    removed second test (ignore)

--- src/screens/StatsScreen.tsx
+++ src/screens/StatsScreen.tsx
@@ -1,8 +1,6 @@
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
 import React from 'react';
 
-// another test
-
 const StatsScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
