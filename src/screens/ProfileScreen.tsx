4300ce7f7db63a9e39a6a17792b9b82575acd149
stevenyang <steventanyang@gmail.com>
2023-11-27 22:28:44 -0500

    profile screen update

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,59 +1,34 @@
 import { StyleSheet, Text, Dimensions, View, SafeAreaView, StatusBar } from 'react-native'
 import React from 'react'
 
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart
-} from "react-native-chart-kit";
-
 
 const ProfileScreen = () => {
-
-  const data = {
-    labels: ["Test1", "Test2"],
-    legend: ["L1", "L2", "L3"],
-    data: [
-      [60, 60, 60],
-      [30, 30, 60]
-    ],
-    barColors: ["#dfe4ea", "#ced6e0", "#a4b0be"]
-  };
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={'light-content'} />
-        <Text style={styles.titleScreen}>Profile</Text>
-      <SafeAreaView style={styles.bigCircle}></SafeAreaView>
 
 
-      <StackedBarChart
-        style={styles.container}
-        data={data}
-        width={400}
-        height={220}
-        hideLegend={true}
-        chartConfig={{
-          backgroundGradientFrom: "#fb8c00",
-          backgroundGradientTo: "#ffa726",
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16,
-          },
-          propsForDots: {
-            r: "6",
-            strokeWidth: "2",
-            stroke: "#ffa726"
-          }
+      <SafeAreaView style={styles.topContainer}>
+        <Text style={styles.topText}>Bronny James</Text>
+        <Text style={[styles.topText, {fontSize: 20, fontWeight: '900'}]}>CO 2023</Text>
+      </SafeAreaView>
 
-        }}
-      />
-    
+      <View style={styles.circleContainer}>
+        <Text style={[styles.topText, {fontSize: 20, color: '#415A77', marginTop: 10}]}>@bronnyjames</Text>
+        <SafeAreaView style={styles.bigCircle}></SafeAreaView>
+      </View>
+
+      <View style={[styles.container, {alignItems: 'center', marginBottom: 0}]}>
+        <View style={[styles.statsContainer, {marginBottom: 0}]}></View>
+      </View>
+
+      <View style={[styles.container, {alignItems: 'center', marginBottom: 0}]}>
+        <View style={[styles.statsContainer, {marginBottom: 0}]}></View>
+      </View>
+
+
+
     </SafeAreaView>
   )
 }
@@ -61,28 +36,50 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0D1B2A',
+    backgroundColor: '#1B263B',
+  },
+
+  topContainer: {
+    justifyContent: 'center',
+    flexDirection: 'row',
     alignItems: 'center',
   },
-  barChart: {
 
-  },
-  titleScreen: {
+  topText: {
     color:'white',
-    fontFamily: 'Arial',
-    fontSize: 32,
-    fontWeight: 'bold',
+    marginRight: 15,
+    marginLeft: 15,
+    marginTop: 20,
+    fontFamily: 'Roboto',
+    fontSize: 30,
+    fontWeight: '900',
+  },
+  circleContainer: {
+    alignItems: 'center',
   },
   bigCircle: {
-    width: 250,
-    height: 250,
+    width: 150,
+    height: 150,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 50,
+    marginTop: 15,
     borderRadius: 150,
     backgroundColor: '#DCDCDC',
     borderColor: "#415A77",
-    borderWidth: 10,
+    borderWidth: 7,
+  },
+  header: {
+    alignItems: 'flex-start',
+    marginLeft: 20,
+  },
+  statsContainer: {
+    backgroundColor: '#0D1B2A',
+    borderRadius: 25,
+    padding: 0,
+    width: '85%', 
+    height: 200, 
+    marginTop: 0,
+
   },
 })
 
