00f3cd214c818224326d27a763c714749304b12c
Zyrephus <wilsonchen315@gmail.com>
2023-11-28 16:27:59 -0500

    Finished year screen (still not sure if to use real data or not)

--- src/screens/YearlyScreen.tsx
+++ src/screens/YearlyScreen.tsx
@@ -9,15 +9,15 @@ const YearlyScreen = () => {
   const data = [
     {
       name: "made",
-      percentage: (yearlydate.shotsmade/yearlydate.shotstaken) * 100,
-      color: "rgba(131, 167, 234, 1)",
+      percentage: 33, //(yearlydate.shotsmade/yearlydate.shotstaken) * 100,
+      color: "#3FA937",
       legendFontColor: "#7F7F7F",
       legendFontSize: 15
     },
     {
       name: "missed",
-      percentage: 100 - ((yearlydate.shotsmade/yearlydate.shotstaken) * 100),
-      color: "#F00",
+      percentage: 66, //100 - ((yearlydate.shotsmade/yearlydate.shotstaken) * 100),
+      color: "#D83030",
       legendFontColor: "#7F7F7F",
       legendFontSize: 15
     }
@@ -31,34 +31,43 @@ const YearlyScreen = () => {
         <Text style={styles.dateText}>2023</Text>
       </View>
 
-      <PieChart
-        data={data}
-        width={300}
-        height={300}
-        style={styles.pieChart}
-        hasLegend={false}
-        chartConfig={{
-          backgroundGradientFrom: "#415A77",
-          backgroundGradientTo: "#415A77",
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          propsForHorizontalLabels: {
-            fontWeight: 'bold',
-          },
-          style: {
-            borderRadius: 16,
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: 900,
-          },
-        }}
-        accessor={"percentage"}
-        backgroundColor={"transparent"}
-        paddingLeft={"70"}
-        center={[0, 0]}
-        absolute
-      />
+      <View style={styles.circle}>
+        <PieChart
+          data={data}
+          width={400}
+          height={400}
+          style={styles.pieChart}
+          hasLegend={false}
+          chartConfig={{
+            backgroundGradientFrom: "#415A77",
+            backgroundGradientTo: "#415A77",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            propsForHorizontalLabels: {
+              fontWeight: 'bold',
+            },
+            style: {
+              borderRadius: 16,
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: 900,
+            },
+          }}
+          accessor={"percentage"}
+          backgroundColor={"transparent"}
+          paddingLeft={"70"}
+          center={[0, 0]}
+          absolute
+        />
+      </View>
+
+      <View style={styles.legendContainer}>
+        <View style={styles.greenCircle}></View>
+        <Text style={styles.legend}>Made</Text>
+        <View style={styles.redCircle}></View>
+        <Text style={styles.legend}>Missed</Text>
+      </View>
     
     </View>
   )
@@ -67,7 +76,6 @@ const YearlyScreen = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#415A77',
-    // alignItems: 'flex-start', // Align content to the left
     paddingTop: 20, // Padding from top
     paddingLeft: 20, // Padding from left
     borderRadius: 40,
@@ -75,9 +83,8 @@ const styles = StyleSheet.create({
   },
 
   pieChart: {
-    marginLeft: 50,
+    marginLeft: 5,
   },
-  
 
   dateText: {
     color:'white',
@@ -94,175 +101,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#0D1B2A',
   },
-
-  topPart: {
-    width: '100%',
-    backgroundColor: 'black',
-  },
-
-  // Weekly Totals
-  weeklyTotals: {
-    color:'white',
-    fontSize: 28,
-    marginTop: 20,
-    fontWeight: 'bold',
-  },
-  
-  statsContainer: {
+  circle: {
+    width: 350,
+    height: 350,
+    justifyContent: 'center',
+    marginLeft: 40,
+    marginBottom: 20,
+    borderRadius: 175,
     backgroundColor: 'white',
-    borderRadius: 25,
-    padding: 20,
-    width: '100%', 
-    height: 205, 
-    marginTop: 15,
-    marginHorizontal: -10,
-    flex: 0, // Prevent the container from growing
   },
-
-  // Daily Charts
-  dailyCharts: {
+  legendContainer: {
+    backgroundColor: '#0D1B2A',
+    // alignItems: 'flex-start', // Align content to the left
+    paddingTop: 20, // Padding from top
+    paddingLeft: 20, // Padding from left
+    borderRadius: 40,
+    flexDirection: 'row',
+    flex: 1,
+  },
+  greenCircle: {
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    marginLeft: 40,
+    marginBottom: 20,
+    borderRadius: 175,
+    backgroundColor: '#3FA937',
+  },
+  redCircle: {
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    marginLeft: 40,
+    marginBottom: 20,
+    borderRadius: 175,
+    backgroundColor: '#D83030',
+  },
+  legend: {
     color:'white',
     fontSize: 28,
-    fontWeight: 'bold',
-    marginTop: 45,
-  },
-
-  chartsContainer: {
-    backgroundColor: '#0D1B2A',
-    borderRadius: 25,
-    padding: 30,
-    width: '100%',
-    height: 205, 
-    marginTop: 15,
-    marginHorizontal: -10,
-    flex: 0, // Prevent the container from growing
-  },
-
-  titleText: {
-    color:'white',
-    fontFamily: 'Roboto',
-    marginTop: 5,
-    marginBottom: 3,
-    fontSize: 25,
-    fontWeight: "800",
-  },
-  
-  smallText: {
-      color:'#415A77',
-      fontFamily: 'Roboto',
-      fontSize: 16,
-      fontWeight: "800",
-  },
-  mainContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 5,
-  },
-  topStatsContainer: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    marginTop: 20,
-    alignItems: 'center',
-  },
-  topStats: {
-    color:'#415A77',
-    fontFamily: 'Roboto',
-    marginTop: 10,
-    marginBottom: 30,
-    marginRight: 7,
-    marginLeft: 7,
-    fontSize: 24,
-    fontWeight: "800",
-  },
-  labelContainer: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  tagsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  tagsText: {
-    color:'#415A77',
-    fontFamily: 'Roboto',
-    marginHorizontal: 50,
-    fontSize: 25,
-    fontWeight: "900",
-  },
-  bottomBoxContainer: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-  },
-  bottomBoxes: {
-    width: 165,
-    height: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderRadius: 25,
-    marginTop: 10,
-    margin: 12,
-    backgroundColor: '#415A77',
-  },
-  columnContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 0,
     marginTop: 0,
+    marginLeft: 10,
+    fontWeight: '800',
   },
-  bigBox: {
-    width: 355,
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    borderRadius: 25,
-    marginTop: 10,
-    margin: 3,
-    backgroundColor: '#415A77',
-  },
-  boxText: {
-    color:'#1B263B',
-    fontSize: 16,
-    fontWeight: "800",
-  },
-  boxRowContainer: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mediumCircles: {
-    width: 80,
-    height: 80,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 150,
-    backgroundColor: '#415A77',
-  },
-  endButton: {
-    width: 250,
-    heigth: 40,
-    borderRadius: 15,
-    alignItems: 'center',
-    backgroundColor: '#415A77',
-    fontFamily: 'Roboto',
-    marginTop: 30,
-    marginBottom: 30,
-  },
-  endButtonText: {
-    color: '#DCDCDC',
-    fontFamily: 'Roboto',
-    fontWeight: '900',
-    fontSize: 20,
-    marginTop: 15,
-    marginBottom: 15,
-  },
-
 })
 
 export default YearlyScreen
